e3852ef3310fe240975ff07a8b07225a914f5b43
Majid Aziz <azizmajid34@gmail.com>
2023-02-22 15:37:08 +0500

    Created Header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/navbar/Navbar'
+import Homepage from './pages/homepage/homepage'
 
 const App = () => {
     return (
         <>
             <Navbar />
+            <Homepage />
         </>
     )
 }
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../Components/header/Header'
+import './homepage.css'
+
+const Homepage = () => {
+    return (
+        <>
+            <Header />
+            homepage
+        </>
+    )
+}
+
+export default Homepage
