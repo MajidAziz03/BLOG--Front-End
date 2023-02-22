e5159bfc26da4a707f41e0c8c2ac0d9d08a0ed09
Majid Aziz <azizmajid34@gmail.com>
2023-02-22 16:16:13 +0500

    Completed Sidebar and posts initiated

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from '../../Components/header/Header'
+import Posts from '../../Components/posts/posts'
+import Sidebar from '../../Components/sidebar/Sidebar'
 import './homepage.css'
 
 const Homepage = () => {
     return (
         <>
             <Header />
-            homepage
+            <div className="home">
+                <Posts />
+                <Sidebar />
+            </div>
         </>
     )
 }
